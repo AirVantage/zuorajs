264974e93e07e83cde17af4f763abe30e450e82b
David Sciamma <dsciamma@sierrawireless.com>
2017-02-03 15:14:13 +0100

    Project setup

---
+++ zuora.js
@@ -0,0 +1,41 @@
+const BPromise = require("bluebird");
+const got = require("got");
+const _ = require("lodash");
+const actionLib = require("./lib/action.js");
+const subscriptionsLib = require("./lib/subscriptions.js");
+
+function Zuora(config) {
+    this.serverUrl = config.url;
+    this.apiAccessKeyId = config.apiAccessKeyId;
+    this.apiSecretAccessKey = config.apiSecretAccessKey;
+    this.entityId = config.entityId;
+    this.entityName = config.entityName;
+
+    this.action = actionLib(this);
+    this.subscriptions = subscriptionsLib(this);
+}
+module.exports = Zuora;
+
+Zuora.prototype.authenticate = function() {
+    if (this.authCookie === undefined) {
+        var self = this;
+        var url = this.serverUrl + "/connections";
+        var query = {
+            headers: {
+                "user-agent": "zuorajs",
+                "Content-type": "application/json",
+                "apiAccessKeyId": this.apiAccessKeyId,
+                "apiSecretAccessKey": this.apiSecretAccessKey
+            },
+            json: true
+        };
+        console.log("Auth request");
+        return got.post(url, query)
+            .then(function(res) {
+                self.authCookie = res.headers["set-cookie"][0];
+                return self.authCookie;
+            });
+    } else {
+        return BPromise.resolve(this.authCookie);
+    }
+};
